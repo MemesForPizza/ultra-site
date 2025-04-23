3fb0038d2c6ca24d55197a0ae73a2ead7451407b
MemesForPizza <76803336+MemesForPizza@users.noreply.github.com>
2025-04-23 10:10:28 +0700

    navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="navbar_root">
+    <div class="navbar_logo_container">
+      <img src="~/assets/images/logo/logo_full.svg">
+    </div>
+    <div class="navbar_contents">
+      <NuxtLink to="/">home</NuxtLink>
+      <NuxtLink class="navbar_link_unavailable">guides</NuxtLink>
+      <NuxtLink class="navbar_link_unavailable">forum</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.navbar_root {
+  background-color: #511515;
+  color: #fff;
+  padding: 1em;
+  display: flex;
+  flex-direction: row;
+
+  .navbar_logo_container {
+    margin-left: 4em;
+    margin-right: 5em;
+    transform: scale(4);
+    image-rendering: pixelated;
+    display: flex;
+    align-items: center;
+    * {
+      height: 1em;
+    }
+  }
+
+  .navbar_contents {
+    text-transform: uppercase;
+
+    :deep(*),
+    :deep(:visited),
+    :deep(:link) {
+      color: #fff;
+      display: inline-block;
+      margin: 0.5em;
+      text-decoration: none;
+    }
+
+    .router-link-active {
+      transform: scale(1.2);
+    }
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit nisi doloribus voluptates repudiandae. Cupiditate officiis tempora esse harum veniam eum tenetur, sit accusantium consectetur et similique voluptate eveniet deleniti eos.
+  </div>
+</template>
