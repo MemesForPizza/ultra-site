8ba26f038d6aebf8d985419d5e6817a2256e1f15
MemesForPizza <76803336+MemesForPizza@users.noreply.github.com>
2025-04-24 12:14:58 +0700

    front pgae

---
+++ components/Section.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="section">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.section {
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  width: calc(100% - 2em);
+  padding: 1em;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,51 @@
 <template>
-  <div>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit nisi doloribus voluptates repudiandae. Cupiditate officiis tempora esse harum veniam eum tenetur, sit accusantium consectetur et similique voluptate eveniet deleniti eos.
+  <div class="page">
+    <Section class="hero_landing">
+      MANKIND IS <span class="red">DEAD</span><br>
+      <span class="red">BLOOD</span> IS FUEL<br>
+      HELL IS <span class="red">FULL</span>
+    </Section>
+    <Section class="section_1">
+      <div class="left">
+        <video src="/videos/trailer.mp4" controls></video>
+      </div>
+      <div class="right">ajaja</div>
+    </Section>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.hero_landing {
+  margin-top: 0;
+  background-image: url("~/assets/images/bg/dark.webp");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 0% 50%;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  font-size: 2em;
+
+  .red {
+    color: #f00;
+  }
+}
+
+.section_1 {
+  display: flex;
+  text-align: left;
+  * {
+    display: flex;
+    justify-content: center;
+  }
+  .left {
+    video {
+      height: 12em;
+    }
+  }
+  .right {
+    justify-content: flex-start;
+    flex-grow: 1;
+  }
+}
+</style>
