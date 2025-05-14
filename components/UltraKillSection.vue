81a7fe64f560c1b30ffbdc9ad027e473e0f820c9
MemesForPizza <76803336+MemesForPizza@users.noreply.github.com>
2025-05-14 07:50:38 +0700

    fix nut ormanemts

--- components/UltraKillSection.vue
+++ components/UltraKillSection.vue
@@ -7,10 +7,9 @@
 <style scoped lang="scss">
 .ultrakill-section {
   border-image-slice: 604 604 604 604;
-  border-image-width: calc(2 * var(--padding, 1em)); 
+  border-image-width: calc(1.5 * var(--padding, 1em)); 
   border-image-source: url("~/assets/images/ornaments/nut.png");
   border-style: solid;
-  border-width: 1px;
   margin-top: 1rem;
   margin-bottom: 1rem;
   width: calc(100% - 2 * var(--padding, 1em) - 2px);
