80d8a0052409756ac4c97d72fd160339f4329513
MemesForPizza <76803336+MemesForPizza@users.noreply.github.com>
2025-05-14 08:01:54 +0700

    setup `@nuxt/content`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ["~/assets/styles/main.scss"],
+  modules: ['@nuxt/content'],
   ssr: true,
   nitro: {
     preset: "cloudflare"
