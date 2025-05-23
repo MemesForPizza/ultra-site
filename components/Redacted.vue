050f9ba0382a449f3a44b17398a7c6a4c4ae7f05
MemesForPizza <76803336+MemesForPizza@users.noreply.github.com>
2025-05-23 14:27:18 +0700

    basic `basics` page content imported from google docs. will add styles later

---
+++ components/Redacted.vue
@@ -0,0 +1,15 @@
+<template>
+  <span class="redacted">
+    <slot>[REDACTED]</slot>
+  </span>
+</template>
+
+<style scoped lang="scss">
+.redacted {
+  background-color: currentColor;
+}
+
+.redacted::selection{
+  background-color: #f00;
+}
+</style>
